bea561940568d164bab2a934c53989ce6dbf9008
Luis Fernando <luistruculo14@gmail.com>
2024-04-29 15:33:13 -0300

    Pedindo nome para o usuário

--- distancia.py
+++ distancia.py
@@ -1,6 +1,9 @@
+nome = input("Digite o nome do motorista: ")
+
 distancia = float(input("Digite a distância da viagem em quilômetros: "))
 velocidade = float(input("Digite a velocidade média da viagem em km/h: "))
 
 tempo = distancia / velocidade
 
-print(f"Para percorrer {distancia} km a uma velocidade média de {velocidade} km/h, levará aproximadamente {tempo:.2f} horas.")
+# Exibir o resultado
+print(f"{nome}, o tempo estimado para percorrer {distancia} km a {velocidade} km/h é de {tempo:.2f} horas.")
