606fcdac2828dc38b401a352f1b115e2df10cc63
Luis Fernando <luistruculo14@gmail.com>
2024-05-07 10:23:47 -0300

    registrando a blueprint

--- api.py
+++ api.py
@@ -1,13 +1,64 @@
 from flask import Blueprint, jsonify, request
+from funcoes import maior_de_50
+from randon_data import *
+from funcoes import *
+from funcoes import *
+from funcoes import *
+from funcoes import *
 
 bp = Blueprint("api", __name__)
+
+@bp.route('/')
+def index():
+    return jsonify({"status": 200, "message": "API do Luis Fernando Truculo Robert"})
+@bp.route("/aleatorios")
+def aleatorios():
+    import random
+    a = random.randint(49, 100)
+    return jsonify({"status": 200, "data": a})
+
+@bp.route("/argumentos/<string:nome>")
+def argumentos(nome: str):
+    return jsonify({"status": 200, "data": nome})
 
 @bp.route("/", methods=("GET,"))
 def index():
     return jsonify({"status": 200, "message": "API Luis"})
 
-@bp.route("/aleatorios", methods=("GET,"))
+@bp.route("/api/aleatorios", methods=("GET,"))
 def aleatorios():
     import random
     a = random.randint(49, 100)
     return jsonify({"status": 200, "data": a})
+
+@bp.route('/A', methods=['GET'])
+def contar_pessoas_maiores_de_50_route():
+    contador = maior_de_50(pessoas)
+    return jsonify({'quantidade': contador})
+
+@bp.route('/B', methods=['GET'])
+def contar_pessoas_mais_de_2000_route():
+    count, porcentagem, total_registros = mais_2000(pessoas)
+    return jsonify({'quantidade': count, 'porcentagem': porcentagem, 'total_registros': total_registros})
+
+@bp.route('/C', methods=['GET'])
+def obter_tres_maiores_salarios():
+    tres_maiores_salarios = []
+    for _ in range(3):
+        pessoa = maior_salario(pessoas, maior=salario_mais_baixo(tres_maiores_salarios))
+        tres_maiores_salarios.append(pessoa)
+def salario_mais_baixo(lista):
+    if len(lista) < 3:
+        return None
+    return min(pessoa['salario'] for pessoa in lista)
+    return jsonify(tres_maiores_salarios)
+
+@bp.route('/D', methods=['GET'])
+def calcular_media_salarial_por_profissao():
+    medias = media_profissoes(pessoas)
+    return jsonify(medias)
+
+@bp.route('/E', methods=['GET'])
+def calcular_intervalo_idades_e_sexo():
+    intervalo_idades, sexo = maior_2000_sexo(pessoas)
+    return jsonify({'intervalo_idades': intervalo_idades, 'sexo': sexo})
