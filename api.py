deba2ff3e1d28786350626f8e288c10494cf1b4c
Luis Fernando <luistruculo14@gmail.com>
2024-05-07 09:06:24 -0300

    criando api

---
+++ api.py
@@ -0,0 +1,13 @@
+from flask import Blueprint
+
+bp = Blueprint("api", __name__)
+
+@bp.route("/", methods=("GET,"))
+def index():
+    return jsonify({"status": 200, "message": "API Luis"})
+
+@bp.route("/aleatorios", methods=("GET,"))
+def aleatorios():
+    import random
+    a = random.randint(49, 100)
+    return jsonify({"status": 200, "data": a})
