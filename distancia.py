60d3fa0a12b7ae7f879e1da40828a2971fd763b3
Luis Fernando <luistruculo14@gmail.com>
2024-05-14 10:07:37 -0300

    alterações aula de merge

--- distancia.py
+++
@@ -1,6 +0,0 @@
-distancia = float(input("Digite a distância da viagem em quilômetros: "))
-velocidade = float(input("Digite a velocidade média da viagem em km/h: "))
-
-tempo = distancia / velocidade
-
-print(f"Para percorrer {distancia} km a uma velocidade média de {velocidade} km/h, levará aproximadamente {tempo:.2f} horas.")
